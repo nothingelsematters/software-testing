7e25a0587a24d763a040cfb3a623f7dd4e5afedd
Simon Naumov <daretoodefy@gmail.com>
2020-10-10 22:53:34 +0300

    :white_check_mark: added simple client tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Everything is rendered", () => {
+
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  it("renders navigation", () => {
+    const navigation = ["Home", "Login", "About"];
+
+    for (var i = 0; i < navigation.length; i++) {
+      const elements = screen.getAllByText(new RegExp(navigation[i], "i"));
+      expect(elements.length).toBeGreaterThan(0);
+      for (var j = 0; j < elements.length; j++) {
+        expect(elements[j]).toBeInTheDocument();
+      }
+    }
+  });
+
+  it("renders home", () => {
+    const element = screen.getByText(/Home page/i);
+    expect(element).toBeInTheDocument();
+  });
+})
