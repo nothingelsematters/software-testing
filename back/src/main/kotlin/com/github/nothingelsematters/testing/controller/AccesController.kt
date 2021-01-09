9e6e7f18beea7e8e1bec5832c03d2d5b4dac0baf
Simon Naumov <daretoodefy@gmail.com>
2021-01-09 15:12:58 +0300

    :rocket: back deploy action

--- back/src/main/kotlin/com/github/nothingelsematters/testing/controller/AccesController.kt
+++ back/src/main/kotlin/com/github/nothingelsematters/testing/controller/AccesController.kt
@@ -3,8 +3,12 @@ package com.github.nothingelsematters.testing.controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
 
 @RestController
 class AccessController {
+    
     @RequestMapping(method = [RequestMethod.OPTIONS, RequestMethod.POST], value = ["/**"]) fun `FOR FUCKS SAKE`() = ""
+
+    @GetMapping("/health") fun healthCheck() = "OK"
 }
